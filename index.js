2eeaeb26fda47f1489228e5665919e023afcfee4
rizaru22 <rizaru.2.2@gmail.com>
2026-01-28 19:21:05 +0700

    iniate project

---
+++ index.js
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs/promises';
+import { GoogleGenAI } from '@google/genai';
+
+const app = express();
+const upload = multer();
+const apiKey=process.env.GOOGLE_API_KEY;
+if (!apiKey) {
+    console.error("API key not found. Please set the GOOGLE_API_KEY environment variable.");
+    process.exit(1);
+}
+const ai=new GoogleGenAI({apiKey});
+
+const GEMINI_MODEL='gemini-2.5-flash';
+const PORT=process.env.PORT || 3000;
+
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server ready on http://localhost:${PORT}`);
+});
+
+app.post('/generate-text', async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:prompt
+            });
+            res.status(200).json({ result:response.text });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+});
