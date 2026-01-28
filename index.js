56f22ed66fb7aebe57a1080650532a4e0b3d41b5
rizaru22 <rizaru.2.2@gmail.com>
2026-01-28 20:09:23 +0700

    ADD End Point generate from document & generate image

--- index.js
+++ index.js
@@ -34,3 +34,43 @@ app.post('/generate-text', async (req, res) => {
         res.status(500).json({message:error.message});
     }
 });
+
+app.post('/generate-image', upload.single('image'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Image = req.file.buffer.toString('base64');
+
+    try{
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:[
+                {type:'text', text:prompt},
+                {inlineData:{data:base64Image, mimeType:req.file.mimetype}}
+            ]
+        });
+        res.status(200).json({ result:response.text });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+});
+        
+app.post('/generate-from-document', upload.single('document'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Document = req.file.buffer.toString('base64');
+    
+    try{
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:[
+                {type:'text', text:prompt??'Tolong buat ringkasan dari dokumen berikut'},
+                {inlineData:{data:base64Document, mimeType:req.file.mimetype}}
+            ]
+        });
+        res.status(200).json({ result:response.text });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:error.message});
+        
+    }
+
+});
